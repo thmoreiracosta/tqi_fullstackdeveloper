6fc3180ec756a5432904093ff0c256d473c819bb
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-27 18:13:44 -0300

    atualizações

--- Módulo 03 - Javascript/08 - desafios/desafio_5 - Contagem de partidas no torneio.js
+++ Módulo 03 - Javascript/08 - desafios/desafio_5 - Contagem de partidas no torneio.js
@@ -10,13 +10,15 @@ let totalMatches = 0
 while (n > 1) {
   if (n % 2 == 0) {
     numberOfMatches = n / 2
-    totalMatches = n / 2
+    n = n / 2
+    totalMatches += numberOfMatches
   } else {
     numberOfMatches = (n - 1) / 2
-    totalMatches = (n - 1) / 2 + 1
+    n = (n - 1) / 2 + 1
+    totalMatches += numberOfMatches
   }
 }
 
-print(numberOfMatches)
+print(totalMatches)
 
 //Escreva o seu código nos espaços em branco
