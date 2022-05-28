8b7e768618d45105b393b84f5163ceb35942f9ef
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-28 01:28:17 -0300

    Conclusão módulo 3

--- Módulo 03 - Javascript/09 - jogo da memória/script.js
+++ Módulo 03 - Javascript/09 - jogo da memória/script.js
@@ -1,9 +1,67 @@
 const cards = document.querySelectorAll('.card')
 
+let hasFlippedCard = false
+let lockBoard = false
+let firstCard, secondCard
+let erros = 0
+
 function flipCard() {
-  this.classList.toggle('flip')
+  if (lockBoard) return
+  if (this === firstCard) return
+
+  this.classList.add('flip')
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true
+    firstCard = this
+    return
+  }
+  secondCard = this
+  lockBoard = true
+  checkForMatch()
 }
 
-cards.forEach(card => {
-  card.addEventListener('click', flipCard)
-})
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+  isMatch ? disableCards() : unflipCards()
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
+
+  resetBoard()
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+
+    resetBoard()
+  }, 1500)
+}
+
+function resetBoard() {
+  ;[hasFlippedCard, lockBoard] = [false, false]
+  ;[firstCard, secondCard] = [null, null]
+}
+
+;(function shuffle() {
+  cards.forEach(card => {
+    let ramdomPos = Math.floor(Math.random() * 20)
+    card.style.order = ramdomPos
+  })
+})()
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
+function refreshPage() {
+  window.location.reload()
+}
+
+
+
+
+
+
