90a347a56dd2b0917ab56a1a3af00d975d4fcdd5
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-17 14:50:51 -0300

    Introdução react - typescripts

--- Módulo 04 - ReactJS/01 - introdução ao reactjs/webpack/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-modules.exports = {
-  entry: "./src/main.tsx",
-  output: {
-    path: path.resolve(__dirname, "bild"),
-    filename: "vundler.tsx",
-  },
-};
---
+++ Módulo_04-ReactJS/01-introduc_reactjs/lesson_1/src/App.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello world!
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
